5ffd609acf99afd73b574ae14eeed207e259b983
taforozi <81076999+taforozi@users.noreply.github.com>
2021-03-31 19:56:37 +0300

    Update view3.sql

--- Views/view3.sql
+++ Views/view3.sql
@@ -1,2 +1,4 @@
+# This view contains all the actors in the system and the characters that they have embodied
+
 SELECT characters.charName AS Characters, name AS Actors
 FROM characters JOIN actors ON characters.charName = actors.charName;
