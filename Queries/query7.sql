49e1fdf2c75bad86eb2ec4f892967775f46e0c97
taforozi <81076999+taforozi@users.noreply.github.com>
2021-03-31 20:18:18 +0300

    Update query7.sql

--- Queries/query7.sql
+++ Queries/query7.sql
@@ -1,3 +1,6 @@
+# Suppose a Marvel fan wants to see the names of the movies, whose cast includes
+# both Robert Downey Jr. and Chris Evans
+
 SELECT movieName AS Movies 
 FROM actors JOIN movies_have_characters ON actors.charName = movies_have_characters.charName
 WHERE actors.name IN ('Robert Downey Jr', 'Chris Evans')
