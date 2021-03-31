ce0d93d03c01b0d674fc74b9e23eab80c8dbaf8b
taforozi <81076999+taforozi@users.noreply.github.com>
2021-03-31 20:08:25 +0300

    Update query3.sql

--- Queries/query3.sql
+++ Queries/query3.sql
@@ -1,3 +1,8 @@
+# Suppose that a Marvel fan is interested in learning about the type and description of events
+# that took place in the first 20, chronologically *, Marvel movies
+
+# *The term chronologically refers to the time of the Marvel world and not to real time
+
 SELECT events.type AS 'Type of event' , events.description AS 'Description of event'
 FROM movies JOIN events ON movies.movieName = events.movieName
 WHERE movies.sequenceOfEvents <= 20;
