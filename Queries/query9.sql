974204b7e70715412d98270bc78bf4c156ab8b24
taforozi <81076999+taforozi@users.noreply.github.com>
2021-03-31 20:24:28 +0300

    Update query9.sql

--- Queries/query9.sql
+++ Queries/query9.sql
@@ -1,3 +1,6 @@
+# Suppose a Marvel fan wants to see all the events and specifically their descriptions, 
+# in which either death or battle has occurred
+
 SELECT description AS 'Event Description' 
 FROM events
 WHERE type = 'death'
