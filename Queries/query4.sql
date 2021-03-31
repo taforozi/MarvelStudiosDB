4dd5ec0381e7bf7f2f898fd5b8c7b57e273bbc2c
taforozi <81076999+taforozi@users.noreply.github.com>
2021-03-31 20:11:37 +0300

    Update query4.sql

--- Queries/query4.sql
+++ Queries/query4.sql
@@ -1,3 +1,6 @@
+# We assume that a Marvel fan is interested in finding out which movies 
+# cost over $250,000,000, as well as their profits 
+
 SELECT movieName AS Movie, budget AS Budget, boxOffice AS BoxOffice
 FROM movies
 WHERE budget > 250000000;
