585b92901ea15aee5f87860799873c13ecbb6559
taforozi <81076999+taforozi@users.noreply.github.com>
2021-03-31 19:55:08 +0300

    Update view2.sql

--- Views/view2.sql
+++ Views/view2.sql
@@ -1,3 +1,5 @@
+# This view contains all the characters who have participated in each event 
+
 SELECT characters.charName AS Characters, events.type AS Type, events.description AS Description
 FROM characters JOIN characters_participatein_events ON characters.charName = characters_charName
     JOIN events ON events_eventID = events.eventID;
